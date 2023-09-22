e748d0e9cd667efa0ce3ba0243d5191bd6ea22e4
Ciarango10 <carlosarango3101@gmail.com>
2023-09-21 19:49:27 -0500

    actualizacion distinct

--- Base de Datos/Distinct.sql
+++ Base de Datos/Distinct.sql
@@ -1,7 +1,3 @@
-SELECT DISTINCT actor.nombre, actor.apellido
-FROM actor
-INNER JOIN actor_pelicula
-ON actor_pelicula.actor_id = actor.id
-INNER JOIN pelicula
-ON pelicula.id = actor_pelicula.pelicula_id
-WHERE pelicula.titulo LIKE "%Harry Potter%"
+-- El resultado mostrará solo los nombres únicos en la columna "Nombre", es decir, no duplicados
+SELECT DISTINCT nombre
+FROM clientes;
