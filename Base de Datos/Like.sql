725d3bb5aa34dddd4c57f68b09a919d86aabbca4
Ciarango10 <carlosarango3101@gmail.com>
2023-09-21 19:53:08 -0500

    Cambios Like

--- Base de Datos/Like.sql
+++ Base de Datos/Like.sql
@@ -1,4 +1,8 @@
 -- Permite especificar algo concreto que queremos encontrar en los registros
-SELECT  telefono
+SELECT telefono
 FROM universolector.editorial
-WHERE razon_social LIKE '%i%'
+WHERE razon_social LIKE '%i%'; -- Devuelve los telefonos donde la razon social tenga una i 
+
+SELECT nombre 
+FROM usuarios 
+WHERE nombre LIKE '_a%'; -- Devuelve los nombres que tengan la letra 'a' como segundo caracter
