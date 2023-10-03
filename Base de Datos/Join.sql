cc2cd0231dc73fb130c8b8809f9b283bb6bf32e8
Ciarango10 <118235923+Ciarango10@users.noreply.github.com>
2023-10-03 07:08:42 -0500

    Update Join.sql

--- Base de Datos/Join.sql
+++ Base de Datos/Join.sql
@@ -4,7 +4,7 @@ FROM cliente
 INNER JOIN factura
 ON cliente.id = factura.cliente_id;
 
--- Devuelve todos los registros donde se cruzan dos o mas tablas. Incluso los registros de una primera tabla que no cumplan la condicion indicada en el ON
+-- Devuelve todos los registros donde se cruzan dos o mas tablas. Incluso los registros de una primera tabla que no cumplan la condicion indicada en el ON.
 SELECT cliente.id, cliente.nombre, factura.fecha
 FROM cliente 
 LEFT JOIN factura
@@ -17,13 +17,13 @@ LEFT JOIN factura
 ON cliente.id = factura.cliente_id
 WHERE factura.id IS NULL;
 
--- Devuelve todos los registros donde se cruzan dos o mas tablas. Incluso los registros de una segunda tabla que no cumplan la condicion indicada en el ON
+-- Devuelve todos los registros donde se cruzan dos o mas tablas. Incluso los registros de una segunda tabla que no cumplan la condicion indicada en el ON.
 SELECT cliente.id, cliente.nombre, factura.fecha
 FROM cliente 
 RIGHT JOIN factura
 ON cliente.id = factura.cliente_id;
 
--- Devuelve los registros de una segunda tabla excluyendo los registros que cumplan con la condicion indicada en el ON
+-- Devuelve los registros de una segunda tabla excluyendo los registros que cumplan con la condicion indicada en el ON.
 SELECT cliente.id, cliente.nombre, factura.fecha
 FROM cliente 
 RIGHT JOIN factura
