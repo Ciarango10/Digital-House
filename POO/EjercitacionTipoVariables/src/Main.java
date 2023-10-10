e389029e6dfb92c9b7d7c8c8b9fe6c71a13d89b2
Ciarango10 <carlosarango3101@gmail.com>
2023-10-09 20:44:29 -0500

    Tipos de datos

---
+++ POO/EjercitacionTipoVariables/src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        String tipoAnimal, nombre, pregunta;
+        int edad;
+        double cantidadComida;
+
+        tipoAnimal = "Perro";
+        nombre = "Manchitas";
+        edad = 2;
+        cantidadComida = 1.5;
+        pregunta = "¡GUAU!";
+        System.out.println("El animal es un " + tipoAnimal + " su nombre es " + nombre + " tiene "
+                + edad + " años, come " + cantidadComida + " kilos, y  hace " + pregunta);
+
+        tipoAnimal = "Pez";
+        nombre = "Nemo";
+        edad = 1;
+        cantidadComida = 0.14;
+        pregunta = "Plop";
+        System.out.println("El animal es un " + tipoAnimal + " su nombre es " + nombre + " tiene "
+                + edad + " año, come " + cantidadComida + " kilos, y  hace " + pregunta);
+
+        tipoAnimal = "Gato";
+        nombre = "Silvestre";
+        edad = 3;
+        cantidadComida = 0.5;
+        pregunta = "¡MIAU!";
+        System.out.println("El animal es un " + tipoAnimal + " su nombre es " + nombre + " tiene "
+                + edad + " años, come " + cantidadComida + " kilos, y  hace " + pregunta);
+
+        tipoAnimal = "Tortuga";
+        nombre = "Manuelita";
+        edad = 12;
+        cantidadComida = 0.3;
+        pregunta = "Pehuajó";
+        System.out.println("El animal es un " + tipoAnimal + " su nombre es " + nombre + " tiene "
+                + edad + " años, come " + cantidadComida + " kilos, y  vive en " + pregunta);
+
+        tipoAnimal = "Pajaro";
+        nombre = "Gardel";
+        edad = 1;
+        cantidadComida = 0.35;
+        pregunta = "¡pío, pío, pío!";
+        System.out.println("El animal es un " + tipoAnimal + " su nombre es " + nombre + " tiene "
+                + edad + " año, come " + cantidadComida + " kilos, y  hace " + pregunta);
+    }
+}
--- POO/TiposDeDatos/src/Main.java
+++ POO/TiposDeDatos/src/Main.java
@@ -2,8 +2,17 @@ public class Main {
     public static void main(String[] args) {
         //DECLARACIÓN DE VARIABLES
         String nombre = "Carlos";
-        int edad = 18;
         double sueldo = 700000.00;
-        System.out.println("Mi nombre es: " + nombre + " tengo " + edad + " años, y mi sueldo es de: " + sueldo);
+        int edad = 18;
+        boolean trabaja = true;
+        System.out.println("Mi nombre es: " + nombre + " tengo " + edad + " años, y mi sueldo es de: " + sueldo + " Trabaja: " + trabaja);
+
+        //SE PUEDEN DECLARAR LAS VARIABLES CON TIPOS DE DATOS COMO CLASES PARA ACCEDER A SUS METODOS
+        Integer edad1 = 2;
+        System.out.println(edad1.equals(2));
+        Double renta = 600.00;
+        System.out.println(renta.compareTo(600.00));
+        Boolean mayorEdad = true;
+        System.out.println(mayorEdad.equals(false));
     }
 }
