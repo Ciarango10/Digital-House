f50976bac4a160342dcdae78215e1bb20e800cc3
Ciarango10 <carlosarango3101@gmail.com>
2024-03-12 20:50:11 -0500

    Promesas

---
+++ FrontEnd-II/Ejercicio Promesas/js/clase-11.js
@@ -0,0 +1,154 @@
+let listadoComentarios = [{
+        postId: 1,
+        id: 1, 
+        name: 'id labore ex et quam laborum',
+        email: 'Eliseo@gardner.biz',
+        body: 'laudantium enim quasi est quidem magnam voluptate …utem quasi\nreiciendis et nam sapiente accusantium'
+    },
+    {
+        postId: 1,
+        id: 2,
+        name: 'quo vero reiciendis velit similique earum',
+        email: 'Jayne_Kuhic@sydney.com',
+        body: 'est natus enim nihil est dolore omnis voluptatem n…iatur\nnihil sint nostrum voluptatem reiciendis et'
+    }, {
+        postId: 1,
+        id: 3,
+        name: 'odio adipisci rerum aut animi',
+        email: 'Nikita@garfield.biz',
+        body: 'quia molestiae reprehenderit quasi aspernatur\naut …mus et vero voluptates excepturi deleniti ratione'
+    }, {
+        postId: 1,
+        id: 4,
+        name: 'alias odio sit',
+        email: 'Lew@alysha.tv',
+        body: 'non et atque\noccaecati deserunt quas accusantium u…r itaque dolor\net qui rerum deleniti ut occaecati'
+    }, {
+        postId: 1,
+        id: 5,
+        name: 'vero eaque aliquid doloribus et culpa',
+        email: 'Hayden@althea.biz',
+        body: 'harum non quasi et ratione\ntempore iure ex volupta…ugit inventore cupiditate\nvoluptates magni quo et'
+    }, {
+        postId: 2,
+        id: 6,
+        name: 'et fugit eligendi deleniti quidem qui sint nihil autem',
+        email: 'Presley.Mueller@myrl.com',
+        body: 'doloribus at sed quis culpa deserunt consectetur q…utem\nvoluptatem repellendus aspernatur dolorem in'
+    }, {
+        postId: 2,
+        id: 7,
+        name: 'repellat consequatur praesentium vel minus molestias voluptatum',
+        email: 'Dallas@ole.me',
+        body: 'maiores sed dolores similique labore et inventore … corporis molestiae mollitia quia et magnam dolor'
+    }, {
+        postId: 2,
+        id: 8,
+        name: 'et omnis dolorem',
+        email: 'Mallory_Kunze@marie.org',
+        body: 'ut voluptatem corrupti velit\nad voluptatem maiores…samus maiores\nvoluptates quia aliquid ullam eaque'
+    }, {
+        postId: 2,
+        id: 9,
+        name: 'provident id voluptas',
+        email: 'Meghan_Littel@rene.us',
+        body: 'sapiente assumenda molestiae atque\nadipisci laboru…natur odit sit rem expedita\nquas enim ipsam minus'
+    }, {
+        postId: 2,
+        id: 10,
+        name: 'eaque et deleniti atque tenetur ut quo ut',
+        email: 'Carmen_Keeling@caroline.name',
+        body: 'voluptate iusto quis nobis reprehenderit ipsum ame…s\nnostrum quaerat nulla et accusamus nisi facilis'
+    }, {
+        postId: 3,
+        id: 11,
+        name: 'fugit labore quia mollitia quas deserunt nostrum sunt',
+        email: 'Veronica_Goodwin@timmothy.net',
+        body: 'ut dolorum nostrum id quia aut est\nfuga est invent…s quo est\nut blanditiis quia ut vel ut maiores ea'
+    }
+];
+
+// Tenemos un listado de comentarios como punto de partida. Esto van a funcionar como registros en una base de datos.
+// Vamos a simular conectarnos con una API para recuperar los comentarios y verlos en pantalla.
+
+
+/* -------------------------------------------------------------------------- */
+/*                      [1] FUNCION: Escuchamos el click                      */
+/* -------------------------------------------------------------------------- */
+const boton = document.querySelector("button")
+
+// si el objeto respuesta de api esta vacio
+
+boton.addEventListener("click", () => { 
+    console.log("Click activado");
+
+    var endpoint = "endpoint" // es la url  www.google.com
+    //  endpoint = "endpointMal" // es la url incorrecta
+
+    consultaAsincrona(endpoint)
+    .then( (respuesta) => {
+        console.log(respuesta);
+        renderizarElementos(respuesta);
+    })
+    .catch( (error) => {
+        console.log(error);
+        console.log(error.message);
+    })
+ })
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                      [2] FUNCION: creamos una promesa                      */
+/* -------------------------------------------------------------------------- */
+// Creamos una funcion que retorna una promesa despues de 2,5 segundos.
+// La idea es simular la demora de tiempo en responder de un servidor.
+
+function consultaAsincrona(url) {
+    // checkee info en la api, y verificando que url es la correcta
+    return new Promise( (resolve, reject) => { 
+        // simulacion de espera de respueata a la API
+        setTimeout(() => {
+            // checkar si la url es la correcta
+            if (url == "endpoint") {
+                // si la url es correcta resuelvo la promesa
+                resolve(listadoComentarios)
+            } else {
+                // si la url es incorrecta rechazo la promesa
+                reject({
+                    message: "Consulta rechazada."
+                })
+            }
+        }, 2500);
+     })
+}
+
+
+
+
+/* ----------------------------- Mesa de trabajo ---------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                       [3] FUNCION: Pintar en pantalla                      */
+/* -------------------------------------------------------------------------- */
+// En este caso la consigna será más abierta, se explicitarán los requerimientos
+// pero hay varias maneras de llegar al resultado.
+// 1- Hay que desarrollar esta función para que reciba los comentarios y los muestre en pantalla.
+// 2- La funcion debe ser llamada en el lugar correspondiente.
+// 3- En el HTML hay un comentario creado, el mismo debe ser eliminado de ahí, pero hay que respetar
+// esa estructura de etiquetas para el resto de los comentarios.
+// 4- Para el renderizado podemos utilizar .forEach() pero se valora también intentar
+//  llegar al mismo resultado utilizando .map()
+// Muchos éxitos!
+
+function renderizarElementos(listado){
+    const comentarios = document.querySelector(".comentarios");
+    comentarios.innerHTML = "";
+    // desarrollar la funcion 👇
+    const comentariosRenderizados = listado.map((comentario) => {
+        return `<div class="comentario">
+            <h4>${comentario.email}</h4>
+            <p>${comentario.body}</p>
+        </div>`
+    })
+    comentarios.innerHTML = comentariosRenderizados.join("");
+}
---
+++ FrontEnd-II/Ejercicio Promesas/js/promises.js
@@ -0,0 +1,57 @@
+// en un mundo ideal la api me trae la info casi al instante
+// const getData = () => { 
+//     return listadoComentarios
+//  }
+
+//  console.log(getData());
+// Qué son las promesas? Son un objeto de javascript que me permite evaluar la promesa de recibir algo y que puede tardar en el tiempo,
+// en un mundo real la api puede tardar en traerme la info incluso por mas de un segundo
+// const getData = () => { 
+//     setTimeout(() => {
+//         console.log("Resuelvo peticion");
+//         return listadoComentarios
+//     }, 2500);
+//  }
+
+//  console.log(getData());
+
+
+//  const getData = () => { 
+//     console.log("inicio de petición");
+
+//     return new Promise ((resolve, reject) => { 
+//         setTimeout(() => {
+//             console.log("Resuelvo peticion");
+//             resolve(listadoComentarios)
+//         }, 2500);        
+//      })    
+//  }
+
+//  console.log(getData());
+ 
+// listadoComentarios = [] // simulo que el array respuesta del API esta vacio
+ 
+ const getData = () => { 
+    console.log("inicio de petición");
+
+    return new Promise ((resolve, reject) => { 
+        // diferentes funciones para ir a buscar ese recurso a la API, e inclusive trasformar esa data
+        if (listadoComentarios.length > 0) {
+            setTimeout(() => {
+                console.log("Resuelvo peticion");
+                resolve(listadoComentarios);
+            }, 2500);                    
+        } else { // indicar que el recurso no tiene contenido y tengo que manejar el rechazo de la respuesta
+            console.log("Rechazo promesa");
+            reject( { message: "Recurso no encontrado", status: 404 } );
+        }
+     })    
+ }
+
+// console.log(getData());
+getData()
+.then( (datos) => {
+    console.log(datos);
+})
+.catch((error) => console.log(error));
+
