a1440c0f30e278fe6d75b4147974f205267a7b79
Ciarango10 <carlosarango3101@gmail.com>
2024-06-08 21:59:57 -0500

    WebConfig

--- BackEnd-I/Clinica-MVC/src/main/java/dh/backend/clinicamvc/configuration/WebConfig.java
+++ BackEnd-I/Clinica-MVC/src/main/java/dh/backend/clinicamvc/configuration/WebConfig.java
@@ -6,12 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    public void addCorsMappings(CorsRegistry registry){
-       /*registry.addMapping("/**")
+    /*public void addCorsMappings(CorsRegistry registry){
+       registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500/")
                 .allowedMethods("GET", "POST", "PUT","DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-        */
-    }
+
+    }*/
 }
