0deffda17e505d7e743024d47b60e8d363470a69
Ciarango10 <carlosarango3101@gmail.com>
2024-08-13 07:04:27 -0500

    Pilares del primer entregable

---
+++ FrontEnd-III/Entregable/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ FrontEnd-III/Entregable/vite-project/src/Components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>Hola soy un tarjeta</div>
+  )
+}
+
+export default Card
