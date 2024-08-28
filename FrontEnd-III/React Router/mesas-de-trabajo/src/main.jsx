88310bbbb705e7336e2940e6796288f03c09fd05
Ciarango10 <carlosarango3101@gmail.com>
2024-08-27 22:37:53 -0500

    Adelanto de mesas de trabajo

--- FrontEnd-III/React Router/mesas-de-trabajo/src/main.jsx
+++ FrontEnd-III/React Router/mesas-de-trabajo/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import Contacto from './Contacto'
+import Beer from './Beer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/beer/:id" element={<Beer />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
